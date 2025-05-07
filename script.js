372341c1d83ffc8d56e0e49bfb7464a08929d18b
James Forino <57734046+jforino@users.noreply.github.com>
2025-05-07 13:28:25 -0700

    add nav circles and style using css

--- script.js
+++ script.js
@@ -5,45 +5,83 @@ const imagesDiv = document.getElementById('images');
 const leftArrow = document.getElementById('left-arrow');
 const rightArrow = document.getElementById('right-arrow'); 
 
+const navCirclesDiv = document.getElementById('nav-circles-div'); 
+
+let currentNavCircleCount = 0;
+let currentNavCircleHighlighted = navCirclesDiv.children[0]; 
+currentNavCircleHighlighted.classList.add('selected'); 
+
 let currentPictureCount = 0; 
 let currentPictureShowing = imagesDiv.children[0]; 
 currentPictureShowing.classList.remove('hidden'); 
 
+const circleToPictureObject = {
+    'circle-1' : 0,
+    'circle-2' : 1,
+    'circle-3' : 2,
+    'circle-4' : 3,
+}
+
+const navCirclesDivArray = Array.from(navCirclesDiv.children); 
+
+navCirclesDivArray.forEach(element => {
+    element.addEventListener('click', () => {
+        switch(element.id)
+
+        console.log(element.id);
+    })
+})
+
+
+
 
 leftArrow.addEventListener('click', () => {
     currentPictureShowing.classList.add('hidden');
+    currentNavCircleHighlighted.classList.remove('selected'); 
     if(currentPictureCount > 0){
         currentPictureCount -= 1;
+        currentNavCircleCount -= 1;
         console.log(currentPictureCount); 
     }
     else if (currentPictureCount == 0){
         currentPictureCount = 3; 
+        currentNavCircleCount = 3;
         console.log(currentPictureCount);
     } 
     currentPictureShowing = imagesDiv.children[currentPictureCount]; 
-    console.log(currentPictureShowing); 
+    
+    currentNavCircleHighlighted = navCirclesDiv.children[currentNavCircleCount];
+    currentNavCircleHighlighted.classList.add('selected');
+    console.log(currentNavCircleHighlighted);
     currentPictureShowing.classList.remove('hidden'); 
 })
 
 rightArrow.addEventListener('click', () => {
     currentPictureShowing.classList.add('hidden');
+    currentNavCircleHighlighted.classList.remove('selected'); 
     if(currentPictureCount < 3){
         currentPictureCount += 1;
+        currentNavCircleCount += 1; 
         console.log(currentPictureCount); 
     }
     else if (currentPictureCount = 3){
         currentPictureCount = 0;
+        currentNavCircleCount = 0;
         console.log(currentPictureCount); 
     }
+    currentNavCircleHighlighted = navCirclesDiv.children[currentNavCircleCount];
+    currentNavCircleHighlighted.classList.add('selected');
     currentPictureShowing = imagesDiv.children[currentPictureCount]; 
     currentPictureShowing.classList.remove('hidden'); 
 })
 
 
-
 dropDownToggleButton.addEventListener('click', () => {
     if(dropDownContent.classList.contains('hidden')){
         dropDownContent.classList.remove('hidden'); 
+    }
+    else {
+        dropDownContent.classList.add('hidden'); 
     }
    
 })
